7070767d1918d41ee8c9cff7cd7224cd2b1b166c
Jose Carlos <josealarconchacon@gmail.com>
2020-12-04 11:09:17 -0500

    First commit

---
+++ FilmTest/FilmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmTest
+//
+//  Created by Jose Alarcon Chacon on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
