f1bc3475526b0cf26a776f5b20ac6e5da28fb431
Jose Carlos <josealarconchacon@gmail.com>
2020-12-11 16:24:43 -0500

    add, remove and query movie item from two different arrays

--- FilmTest/FilmTest/Models/MovieManager.swift
+++ FilmTest/FilmTest/Models/MovieManager.swift
@@ -10,9 +10,12 @@ import Foundation
 
 class MovieManager {
     var movieToSeeCount = 0
-    let movieSeenCount = 0
+    var movieSeenCount = 0
     
     private var movieToSeeArray = [Movie]()
+    
+    // private array that keep track of the movie that we check off to seen
+    private var movieSeenArray = [Movie]()
     
     
     func addMoview(movie: Movie) {
@@ -22,5 +25,19 @@ class MovieManager {
     
     func addMovieAtIndex(index: Int) -> Movie {
         return movieToSeeArray[index]
+    }
+    
+    func checkOffMovieAtIndex(index: Int) {
+        // modify our count variable by decreasing movieToSeeCount  by 1 and increment movieSeenCount by 1
+        guard index < movieToSeeCount else { return }
+        movieToSeeCount -= 1
+        movieSeenCount += 1
+        
+        let checked_movie = movieToSeeArray.remove(at: index)
+        movieSeenArray.append(checked_movie)
+    }
+    
+    func checkedOffMovieAtIndex(index: Int) -> Movie{
+        return movieSeenArray[index]
     }
 }
